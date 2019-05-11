0352cea5dfb7f38e08fe462e85b3908a3226f59d
JungTa <quddnr0708@gmail.com>
2019-05-11 20:01:49 +0900

    post 컨트랙트 구성중

--- contracts/Post.sol
+++ contracts/Post.sol
@@ -1,16 +1,36 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract Post {
+contract PostDB {
 
-    struct post {
+    struct Post {
         string title;
         string content;
         string tag;
+        uint256 publishedDate;
+        bool removed;
     }
     
-    mapping(uint256 => post) posts;
+    // mapping(uint256 => post) public posts;
+    Post[] public posts;
 
     constructor() public {
 
     }
+
+    event SetPost(string indexed title, string content, string tag, uint256 publishedDate);
+    
+    function setPost(string memory title, string memory content, string memory tag) public {
+        posts.push(Post(
+            title,
+            content,
+            tag,
+            now,
+            false
+        ));
+    }
+
+    function getPostLen() public view returns(uint256) {
+        return posts.length;
+    }
+
 }
